aff82db97144a8a7331157994a50b190f83f5e49
H_hammer/ヒロくん <114934468+h-hammer001@users.noreply.github.com>
2025-02-01 16:05:04 +0900

    Update source_append.js

--- source/source_append.js
+++ source/source_append.js
@@ -77,7 +77,7 @@ const sause_append = [
 {id:76,name:"snooze(APD)",level:"a33",type:[5,9,11,12,22,23]},
 {id:77,name:"のだ(APD)",level:"a33",type:[4,10,11,12,22,23,24]},
 {id:78,name:"仮死化(APD)",level:"a28",type:[3,4,9]},
-{id:79,name:"ワンスアポンアドリーム(APD)",level:"a28",type:[3,5,8,21]}/*,
-{id:80,name:"「1」(APD)",level:"a28",type:[]},
+{id:79,name:"ワンスアポンアドリーム(APD)",level:"a28",type:[3,5,8,21]},
+{id:80,name:"「1」(APD)",level:"a28",type:[19]}/*,
 {id:81,name:"ハッピーシンセサイザ(APD)",level:"a28",type:[]},*/
 ]
