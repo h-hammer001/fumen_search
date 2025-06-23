b8348e2a519c20cdfb4c400f96fb57d88f0a7ccf
H_hammer/ヒロくん <114934468+h-hammer001@users.noreply.github.com>
2025-06-23 18:36:00 +0900

    Update source_append.js

--- source/source_append.js
+++ source/source_append.js
@@ -98,6 +98,7 @@ const sause_append = [
 {id:96,name:"フラジール(APD)",level:"a28",type:[9,12,14]},
 {id:97,name:"とても痛い痛がりたい(APD)",level:"a30",type:[10,12,13]},
 {id:98,name:"テトリス(APD)",level:"a29",type:[5,9,10,23,24]},
-{id:,name:"トワイライトライト(APD)",level:"a27",type:[4,19]}/*,
+{id:99,name:"トワイライトライト(APD)",level:"a27",type:[4,19]},
+{id:100,name:"インテグラル(APD)",level:"a29",type:[12,27]}/*,
 {id:,name:"(APD)",level:"a28",type:[]},*/
 ]
